91af40e42985d43a67cf2f4573de6788e83ea293
SickleTM <78701216+SickleTM@users.noreply.github.com>
2021-05-01 16:48:56 +0200

    Update passgen.py

--- passgen.py
+++ passgen.py
@@ -1,13 +1,15 @@
 import random
 
 chars = "abcdefghijklmnopqrstuwxyzABCEFGHIJKLMNOPQRSTUVWXYZ1234567890#!\{}|-_=%^$*:/.(`)"
+chars2 = "+#^@#%$+_-$=%=@+!@!#@+-_*-=--@!!$*=$*%^-@=*&**+@*#=_++-!^@===%^&==!=&$%_-_%-_+#*#_#=_@*&*@*++%!!^_*!"
 
 if 1:
-    password_len = int(input("What length would you like ? : "))
-    password_count = int(input("How many password you would ? :"))
-    for x in range(0,password_count):
+    password_length = int(input("What length would you like ? : "))
+    password_c = int(input("How many password you would ? :"))
+    for x in range(0,password_c):
         password = ""
-        for x in range(0,password_len):
-            password_char = random.choice(chars)
+        for x in range(0,password_length):
+            password_char = random.choice(chars + chars2)
             password = password + password_char
-        print("Here is your password :", password)
+        print(password)
+
